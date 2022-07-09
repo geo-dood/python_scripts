72a85ac721498e56491473eabef847fdd9e87937
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-09 03:54:10 -0500

    Commit for first Python Script exercise

---
+++ week2/first-script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Author: George Maysack-Schlueter
+# Description: Python script - will output some info Im learning in class.
+
+
+# Creating a variable set to my full name
+myFullName = "George D. Maysack-Schlueter"
+
+# Printing greeting w/ full name variable, then one thing I learned this module
+print(f"Hello, my full name is {myFullName}!")
+print("One thing I learned about Python in this module is that we can use the 'try' and 'except' statements in conjunction  with one another to verify the user input is the data type we intended it to be.")
