d00a4ea6c60be09e113aa789b937f9ea37df9726
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-13 01:18:45 -0500

    Ready for Grading: Boolean Logic Basics Lab

---
+++ week3/boolean.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#Author: George Maysack-Schlueter
+#Description: This script is for the Boolean Logic Basics Labs from week 3. We are testing the output of the booleans provided in the lab document
+
+# Printing the results from the 19 provided boolean expressions. 
+print(True and True)
+print(False and True)
+print(1 == 1 and 2 == 1)
+print(bool(0))
+print(bool(""))
+print(bool(0.0))
+print(not 0)
+print('test' == 'test')
+print(1 == 1 or 2 != 1)
+print(True and 1 == 1)
+print(False and 0 != 0)
+print(True or 1 == 1)
+print('test' == 'testing')
+print(1 != 0 and 2 == 1)
+print('test' != 'testing')
+print('test' == 1)
+print(1 == 1 and not ('testing' ==1 or 1 ==0))
+print('chunky' == 'bacon' and not (3 == 4 or 3 == 3))
+print(3 == 3 and not ('testing' == 'testing' or 'Python' == 'Fun'))
