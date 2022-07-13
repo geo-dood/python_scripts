0d4ce370de1dfc03c954a96b39f8dd85ad566e74
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-12 23:39:16 -0500

    READY FOR GRADING - FILE DATA INTERACTIONS

--- week3/files.py
+++ week3/files.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 #Author: George Maysack-Schlueter
 # Description: This is a script that will be applying concepts from week 3 of python scripting. This is for the "File Data Interactions" Lab
 
@@ -12,7 +11,6 @@ print(len(etcPassStr))
 print("^ Here, the len() function is counting the amount of characters in the /etc/passwd file when read as a single string")
 print("***Use this when you want to view your file as a single string, or to view the total amount of characters in the file***")
 print()
-
 # Navigating back to start of /etc/passwd file, then using readlines to read /etc/passwd as a list, which stores each line as a string in said list.
 etcPass.seek(0,0)
 etcPassList = etcPass.readlines()
@@ -20,10 +18,10 @@ print(len(etcPassList))
 print("^ Here, the len() function is counting the amount of items (strings) in the list version of our /etc/passwd file. This is also the amount of lines in the file.")
 print("***Use this when you want to end up with a separated list of items. Each line will generate an item in the created list. \n Can be converted to string format - can be used in for loops, as well***")
 print()
-
 # Closing /etc/passwd file
 etcPass.close()
 
+# Using a Function & a Loop to access and save /etc/passwd contents line by line, and printing number of characters. 
 """
 Using with open method to create while loop, and printing the final value of our "lineInfo" variable - which is the number of characters in the file. 
 In this instance, we did not need to use the len() function, because the final value of our tell() function is indicative of the number of characters. 
@@ -40,5 +38,4 @@ with open("/etc/passwd", "r") as etcPassLine:
         line = etcPassLine.readline()
     print(lineInfo)
 print("^ Here, the tell () function is tracking the position of our pointer in the /etc/passwd file - our while loop continues until the final character is hit, at which \n point, we simply print the final value of our position in the file")
-print("***This is useful when we want to read, and process one line at a time. With our 'readlines' example above, the entire contents of /etc/passwd \n were turned into a list in one step. \n in this example, our while look stepped through one line at a time until \n it reached the end of the file - similar output, but different use cases***")
-    
+print("***Useful when we want to process one line at a time. With our example above, the entire contents of /etc/passwd \n were stored into a list in one step. \n in this example, our while look stepped through one line at a time until \n it reached the end of the file - similar output, but different use cases***")
