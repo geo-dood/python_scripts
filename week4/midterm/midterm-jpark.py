27de64d018f1564a38740d823acadb9fd5d801ba
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 02:44:03 -0500

    Initial commit for Jurassic park section

---
+++ week4/midterm/midterm-jpark.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Author: George Maysack-Schlueter
+# Description: This is a script for week 4 of python scripting class. This is for the midterm project " A python in jurrasic park" section. 
+
+print("George Maysack-Schlueter")
