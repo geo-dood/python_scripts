24ee1687a1b3524ed34c688312fe3e12d57e14c3
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 07:39:50 -0500

    Working on slicing portion of midterm

--- week4/midterm/midterm-slicing.py
+++ week4/midterm/midterm-slicing.py
@@ -4,4 +4,18 @@
 # Description: This is a script for week 4 of python scripting class. This is for the midterm script "Slicing"
 
 print("George Maysack-Schlueter")
+print("")
 
+sliceFile = open("/home/student/Documents/scripts-geo-dood/week4/midterm/slicing-file.txt", "r")
+thirdFromEnd = print(f"{sliceFile.read()}"[126:133:1])
+sliceFile.seek(0,0)
+thirdThroughFifth = print(f"{sliceFile.read()}"[16:29:1])
+sliceFile.seek(0,0)
+tenthFromEndPlusTwo = print(f"{sliceFile.readlines()[17:12:-2]}")
+sliceFile.seek(0,0)
+elevenThroughThirteen = print(f"{sliceFile.read()}"[57:72:1])
+sliceFile.seek(0,0)
+nineTeenThroughTwentyOneFromEnd = print(f"{sliceFile.readlines()[8:5:-1]}")
+sliceFile.seek(0,0)
+quote = print(f"Hello {thirdFromEnd}")
+sliceFile.close()
