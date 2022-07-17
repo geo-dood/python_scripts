7be33291fe79db3ef8d09539944cf127255fd728
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 02:24:18 -0500

    Ready for Grading: Midterm-IF Question

--- week4/midterm/midterm-if.py
+++ week4/midterm/midterm-if.py
@@ -4,9 +4,6 @@
 # Description: This is a script for week 4 of python scripting class. This is the first of three scripts for the Midterm. This script is for the "Midterm-if [Question]"
 
 # Per step 2, I am printing out my first and last name to the screen. 
-from fileinput import lineno
-
-
 print("Name: George Maysack-Schlueter")
 
 # Setting up variable to keep track of line number sum, one to actually move the for loop forward, and a list of strings of the keywords we are searching for.
