2347f051ccf5ed3e03b08ba37c853d2f6a58e9c0
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 05:54:53 -0500

    Initial commit for Slicing portion of midterm

---
+++ week4/midterm/midterm-slicing.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Author: George Maysack-Schlueter
+# Description: This is a script for week 4 of python scripting class. This is for the midterm script "Slicing"
+
+print("George Maysack-Schlueter")
+
