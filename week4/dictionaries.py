89716220884165578fcd147cdd04feed949e4ed3
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 00:03:52 -0500

    READY FOR GRADING: DICTIONARIES LAB

--- week4/dictionaries.py
+++ week4/dictionaries.py
@@ -7,25 +7,25 @@
 dictionary = {"server1.testlab.com":"192.168.1.10","server2.testlab.com":"192.168.1.11","server3.testlab.com":"192.168.1.12","server4.testlab.com":"192.168.1.13",\
               "server5.testlab.com":"192.168.1.14","server6.testlab.com":"192.168.1.15","server7.testlab.com":"192.168.1.16","server8.testlab.com":"192.168.1.17"}
 
-# Printing dictionary keys. Accessing keys with <dictionaryname>.keys() and putting that inside of a print function. Empty print statements added for formatting. 
+# Printing dictionary keys (FQDN's in this case). Accessing keys with <dictionaryname>.keys() and putting that inside of a print function. Empty print statements added for formatting. 
 print("")
 print(f"KEYS: {dictionary.keys()}")
 print("")
 # Printing dictionary values (IP addresses in this case). Accessing values with <dictionaryname>.values() and putting that inside of a print function. 
 print(f"IP ADDRESSES: {dictionary.values()}")
 print("")
-# Printing dictionary keys and values together. Accessing key/value pairs with <dictionaryname>.items() and putting that inside of a print function. 
+# Printing dictionary keys and values together (FQDN + IP Address in this case). Accessing key/value pairs with <dictionaryname>.items() and putting that inside of a print function. 
 print(f"FULL RECORDS: {dictionary.items()}")
 print("")
 #using an if statement to check if server2.testlab.com exists in our dictionary - which will then print to screen whether it appears or not. 
 if "server2.testlab.com" in dictionary:
-    print("server2.testlab.com was found in dictionary!")
+    print("FQDN 'server2.testlab.com' was found in dictionary!")
 else:
-    print("server2.testlab.com was not found in dictionary!")
+    print("FQDN 'server2.testlab.com' was not found in dictionary!")
 print("")
 #using an if statement to check if server10.testlab.com exists in our dictionary - which will then print to screen whether it appears or not. 
 if "server10.testlab.com" in dictionary:
-    print("server10.testlab.com was found in dictionary!")
+    print("FQDN 'server10.testlab.com' was found in dictionary!")
 else:
-    print("server10.testlab.com was not found in dictionary!")
+    print("FQDN 'server10.testlab.com' was not found in dictionary!")
 print("")
