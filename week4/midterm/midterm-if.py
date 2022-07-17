d13d4d32bd1b1bb86876c6c11bfe4a954efeaf4e
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 00:51:56 -0500

    Working on Midter-if Question

--- week4/midterm/midterm-if.py
+++ week4/midterm/midterm-if.py
@@ -5,3 +5,12 @@
 
 # Per step 2, I am printing out my first and last name to the screen. 
 print("Name: George Maysack-Schlueter")
+
+Total = 0
+with open("Midterm-if.txt", "rb") as midtermif:
+    line = midtermif.read(10)
+    while line:
+        lineInfo = f"{count:03d}) [Length:{len(line):03d}] [Index: {midtermif.tell():04d}] {line}"
+        print(lineInfo.strip())
+        count += 1
+        line = midtermif.readline()
