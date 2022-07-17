09b648f08d37de0154f81e31464e2c1479fc861e
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 00:32:30 -0500

    Midterm Initial Commit

---
+++ week4/midterm/midterm-if.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Author: George Maysack-Schlueter
+# Description: This is a script for week 4 of python scripting class. This is the first of three scripts for the Midterm. This script is for the "Midterm-if [Question]"
+
+# Per step 2, I am printing out my first and last name to the screen. 
+print("Name: George Maysack-Schlueter")
