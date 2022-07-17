4b4d0edec710a70de58218e1bf1e3d73b2e4d196
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-16 23:59:50 -0500

    READY FOR GRADING - DICTIONARIES LAB

---
+++ week4/dictionaries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Author: George Maysack-Schlueter
+# Description: This is a script for week 4 of python scripting. This script will be applying concepts we learned regarding dictionaries in python.
+
+# defining our dictionary with domain names/Ip addresses. Line break used for readability
+dictionary = {"server1.testlab.com":"192.168.1.10","server2.testlab.com":"192.168.1.11","server3.testlab.com":"192.168.1.12","server4.testlab.com":"192.168.1.13",\
+              "server5.testlab.com":"192.168.1.14","server6.testlab.com":"192.168.1.15","server7.testlab.com":"192.168.1.16","server8.testlab.com":"192.168.1.17"}
+
+# Printing dictionary keys. Accessing keys with <dictionaryname>.keys() and putting that inside of a print function. Empty print statements added for formatting. 
+print("")
+print(f"KEYS: {dictionary.keys()}")
+print("")
+# Printing dictionary values (IP addresses in this case). Accessing values with <dictionaryname>.values() and putting that inside of a print function. 
+print(f"IP ADDRESSES: {dictionary.values()}")
+print("")
+# Printing dictionary keys and values together. Accessing key/value pairs with <dictionaryname>.items() and putting that inside of a print function. 
+print(f"FULL RECORDS: {dictionary.items()}")
+print("")
+#using an if statement to check if server2.testlab.com exists in our dictionary - which will then print to screen whether it appears or not. 
+if "server2.testlab.com" in dictionary:
+    print("server2.testlab.com was found in dictionary!")
+else:
+    print("server2.testlab.com was not found in dictionary!")
+print("")
+#using an if statement to check if server10.testlab.com exists in our dictionary - which will then print to screen whether it appears or not. 
+if "server10.testlab.com" in dictionary:
+    print("server10.testlab.com was found in dictionary!")
+else:
+    print("server10.testlab.com was not found in dictionary!")
+print("")
