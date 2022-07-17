78fd094cc2e31015f257242b8ea010d151230773
George D. Maysack-Schlueter <gdmaysack@madisoncollege.edu>
2022-07-17 08:19:18 -0500

    working on slicing portion

--- week4/midterm/midterm-slicing.py
+++ week4/midterm/midterm-slicing.py
@@ -7,15 +7,16 @@ print("George Maysack-Schlueter")
 print("")
 
 sliceFile = open("/home/student/Documents/scripts-geo-dood/week4/midterm/slicing-file.txt", "r")
-thirdFromEnd = print(f"{sliceFile.read()}"[126:133:1])
+thirdFromEnd = sliceFile.read()[126:133:1]
 sliceFile.seek(0,0)
-thirdThroughFifth = print(f"{sliceFile.read()}"[16:29:1])
+thirdThroughFifth = sliceFile.read()[16:29:1]
 sliceFile.seek(0,0)
-tenthFromEndPlusTwo = print(f"{sliceFile.readlines()[17:12:-2]}")
+tenthFromEndPlusTwo = sliceFile.readlines()[17:12:-2]
 sliceFile.seek(0,0)
-elevenThroughThirteen = print(f"{sliceFile.read()}"[57:72:1])
+elevenThroughThirteen = sliceFile.read()[57:72:1]
 sliceFile.seek(0,0)
-nineTeenThroughTwentyOneFromEnd = print(f"{sliceFile.readlines()[8:5:-1]}")
+nineTeenThroughTwentyOneFromEnd = sliceFile.readlines()[8:5:-1]
 sliceFile.seek(0,0)
-quote = print(f"Hello {thirdFromEnd}")
+rawquote = f"{thirdFromEnd} {thirdThroughFifth} {tenthFromEndPlusTwo} {elevenThroughThirteen} {nineTeenThroughTwentyOneFromEnd}".replace("\n"," ").replace("\\n"," ").replace(" ['"," ")
+print(rawquote)
 sliceFile.close()
