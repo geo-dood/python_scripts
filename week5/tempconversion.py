d99d3a5d063cd3f13a79de0cad34f1c02fe8d71e
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-25 06:06:49 -0500

    Ready for Grading: Functions Lab

---
+++ week5/tempconversion.py
@@ -0,0 +1,10 @@
+#!/user/bin/env python3
+
+# Author: George Maysack-schlueter 
+# Description: Script for week 5 in python scripting class. This script is for the second script in the functions lab ' Temp Conversion'
+
+import f2c
+
+user_input = input("Enter Degrees in Fahrenheit to be Converted to Celsius: ")
+
+print(f2c.convert_temp(user_input))
