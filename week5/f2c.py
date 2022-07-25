890e6397cb758d70a942a0357ee3ab4becbeeddc
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-25 05:44:29 -0500

    commit for script 1

---
+++ week5/f2c.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python3
+
+# Author: George Maysack-schlueter 
+# Description: Script for week 5 in python scripting class. This script is for the first script in the functions lab ' Convert Fahrenheit to Celsious'
+
+def convert_temp(degrees_fahrenheit):
+    degrees_celsius = (degrees_fahrenheit - 32) * 5/9
+    return degrees_celsius
+
+def main():
+    print(convert_temp(32))
+
+if __name__ == "__main__":
+    main()
