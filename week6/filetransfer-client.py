b3744ed3e18633e18c651634898a1b17ca0fd691
geo-dood <gdmaysack@madisoncollege.edu>
2022-07-30 19:22:53 -0500

    Ready for grading: File transfer client and server

--- week6/filetransfer-client.py
+++ week6/filetransfer-client.py
@@ -5,3 +5,21 @@
 #***THIS IS THE FILE TRANSFER CLIENT***
 
 import socket
+
+RHOST = '127.0.0.1'  # The server IP address
+RPORT = 5001  # The target port as used by the server
+
+# Letting socket know to communicate with the chat server using IPv4 and TCP
+C_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+C_SOCK.connect((RHOST, RPORT))
+
+targetFile = open("transferFile.txt", "rb")
+fileTransfer = targetFile.read(4096)
+
+while fileTransfer:
+    C_SOCK.send(fileTransfer)
+    fileTransfer = targetFile.read(4096)
+
+C_SOCK.close()
+
+print("File transfer complete!")
--- week6/filetransfer-server.py
+++ week6/filetransfer-server.py
@@ -5,3 +5,31 @@
 #***THIS IS THE FILE TRANSFER SERVER***
 
 import socket
+
+# Local host
+LHOST = '127.0.0.1'
+LPORT = 5001
+
+# Initializing connection with the chat client using IPv4 and TCP
+L_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+L_SOCK.bind((LHOST, LPORT))
+
+
+L_SOCK.listen(1)
+
+serverFile = open('received.txt', 'wb')
+print("\n Copied file name will be 'received.txt' on server-side\n")
+
+while True:
+    L_CONN, addr = L_SOCK.accept()
+
+    RCV_DATA = L_CONN.recv(4096)
+    while RCV_DATA:
+        serverFile.write(RCV_DATA)
+        RCV_DATA = L_CONN.recv(4096)
+
+    serverFile.close()
+    print("\n File successfully copied! \n")
+    # This line sends the data back to the client
+    L_CONN.close()
+    break
