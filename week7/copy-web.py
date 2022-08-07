52f2ac8636eb1358df3df82b5a98ffffef264c2d
geo-dood <gdmaysack@madisoncollege.edu>
2022-08-07 03:51:38 -0500

    Working on week 7 scripts

--- week7/copy-web.py
+++ week7/copy-web.py
@@ -3,3 +3,12 @@
 # Author: George Maysack-Schlueter
 # Description: This is a script for week 7 of python scripting class. This script is for the "interacting with a website" lab.
 
+import requests,bs4
+
+# Step 1 - Copying a webpage and saving it to a file
+import requests
+
+response = requests.get("https://notpurple.com")
+
+with open("my_web_file.html", "w") as hFile:
+    hFile.write(response.text)
