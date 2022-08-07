f14d3c4376b53b9f9be72fa1ba90fc375bea1f10
geo-dood <gdmaysack@madisoncollege.edu>
2022-08-07 05:26:34 -0500

    Ready for Grading: Interacting with a website lab

--- week7/copy-web.py
+++ week7/copy-web.py
@@ -6,11 +6,15 @@
 import requests,bs4
 
 # Step 1 - Copying a webpage and saving it to a file
+#Importing our required module - only requests is needed here.
 import requests
 
+#saving our html response to a variable using the get function
 response = requests.get("https://notpurple.com")
 
+# Opening a file with write accesses, and writing the text response from the webpage above to it before saving
 with open("my_web_file.html", "w") as hFile:
     hFile.write(response.text)
 
+# printing completion message for visibility
 print("Copy page to file complete! Check file 'my_web_file.html for results.")
--- week7/inspect-web.py
+++ week7/inspect-web.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
 
-# Author: George Maysack-Schlueter
-# Description: This is a script for week 7 of python scripting class. This script is for the "interacting with a website" lab.
+# Author: George Maysack-Schlueter Description: This is a script for week 7 of python scripting class. This script is
+# for the "interacting with a website" lab.
 
-import requests,bs4
+# Importing required modules - we will be using requests and beautifulsoup4
+import bs4
+import requests
 
+# Initiating our request to the desired domain
 res = requests.get('http://notpurple.com')
 res.raise_for_status()
 
+# Creating our variable to store the response as text and to use the HTML parser
 myHTML = bs4.BeautifulSoup(res.text, "html.parser")
 
-#printing title of page
-print(f'Here is the Title: {myHTML.title.text}')
-print('___________________________________________________')
+# printing title of page with some formatting
+print('')
+print(f'Here is the Title:\n{myHTML.title.text}\n')
+print('___________________________________________________\n')
 print('Here is the list of links:\n')
-#printing list of links on page
-links = myHTML.select('a')
 
-for link in links:
+# printing list of links on page with some formatting
+# Created a variable to store the returned values from parsing our page for 'a' tags (Links)
+myLinks = myHTML.select('a')
+
+# Using a for loop to say if the href value is NOT equal to NONE, then check if 'https' is in that link - if it
+# is, print the link to the screen.
+for link in myLinks:
     print(link.get_text())
-    if link.get('href') != None:
+    if link.get('href') is not None:
         if 'https://' in link.get('href'):
             print(link.get('href'))
-        else:
-            print(link.get('href'))
+
+print('')
