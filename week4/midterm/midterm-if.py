e9ffc72e0a46c835a9bad719c9252c10d503615d
geo-dood <gdmaysack@madisoncollege.edu>
2022-08-13 00:01:19 -0500

    Cleaning formatting - initial push for python scripts repo

--- week4/midterm/midterm-if.py
+++ week4/midterm/midterm-if.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-# Author: George Maysack-Schlueter
-# Description: This is a script for week 4 of python scripting class. This is the first of three scripts for the Midterm. This script is for the "Midterm-if [Question]"
+# Author: George Maysack-Schlueter Description: This is a script for week 4 of python scripting class. This is the
+# first of three scripts for the Midterm. This script is for the "Midterm-if [Question]"
 
 # Per step 2, I am printing out my first and last name to the screen. 
 print("Name: George Maysack-Schlueter")
 print("")
-# Setting up variable to keep track of line number sum, one to actually move the for loop forward, and a list of strings of the keywords we are searching for.
+# Setting up variable to keep track of line number sum, one to actually move the for loop forward, and a list of
+# strings of the keywords we are searching for.
 Total = 0
 count = 0
 words = ["gmeach18@ed.gov", "248.253.63.149", "Whiteland", "80.222.19.190", "Kayley", "dcassyqw@microsoft.com"]
@@ -15,8 +16,9 @@ with open("/home/student/Documents/scripts-geo-dood/week4/midterm/Midterm-if.txt
     # for each line in midtermif, we are adding 1 to our count, which will keep track of the line number
     for line in midtermif:
         count += 1
-        # for our words in our list, if they are found in the current line, the count number is added to our Total, otherwise, we add nothing to the total. 
-        # we subtract 1 from the count, because our text editors start on line 1, but we start on 0. 
+        # for our words in our list, if they are found in the current line, the count number is added to our Total,
+        # otherwise, we add nothing to the total. we subtract 1 from the count, because our text editors start on
+        # line 1, but we start on 0.
         for word in words:
             if word in line:
                 Total += count - 1
